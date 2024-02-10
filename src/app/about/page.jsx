c374c336a729ed9d130bd23bec5bd7a83d412d6c
felixNyalenda <felixasejunior@gmail.com>
2024-02-10 13:19:46 +0300

    brain motion

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,15 @@
 "use client";
 
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
 
 import { skills } from "./data";
+import { Brain } from "@/components";
 
 const About = () => {
+  const containerRef = useRef();
+
+  const { scrollYProgress } = useScroll({ container: containerRef });
   return (
     <motion.div
       className="h-full"
@@ -13,7 +17,7 @@ const About = () => {
       animate={{ y: 0 }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full overflow-scroll lg:flex">
+      <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
         <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2">
           {/* Biography */}
           <div className="flex flex-col gap-12 justify-center">
@@ -209,7 +213,9 @@ const About = () => {
           </div>
         </div>
 
-        <div className="hidden lg:block w-1/3 xl:w-1/2"></div>
+        <div className="hidden lg:block w-1/3 xl:w-1/2 sticky top-0 z-30">
+          <Brain scrollYProgress={scrollYProgress} />
+        </div>
       </div>
     </motion.div>
   );
